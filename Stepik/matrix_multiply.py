2758f46e5b54f8307b8d73daadb7cfaa0bb3b3c6
QvasiM0rda <darkrustam@gmail.com>
2022-09-05 06:52:55 +0300

    Updated matrix_multiply.py - needs to be improved

--- Stepik/matrix_multiply.py
+++ Stepik/matrix_multiply.py
@@ -1,9 +1,21 @@
 def fill_matrix(rows):
     return [[int(i) for i in input().split()] for _ in range(rows)]
 
+def flip_matrix(matrix, rows, cols):
+    result = [[0] * rows for _ in range(cols)]
+    for i in range(rows):
+        for j in range(cols):
+            result[j][i] = matrix[i][j]
+    return result
+
 # Доработать
 def mult_matrix(matrix1, matrix2, rows1, cols1, rows2, cols2):
-    pass
+    result = [[0] * cols2 for _ in range(rows1)]
+    for i in range(rows1):
+        for j in range(cols1):
+            for k in range(cols2):
+                result[i][j] += matrix1[i][k] * matrix2[j][k]
+    return result
 
 def print_matrix(matrix):
     for row in matrix:
@@ -13,5 +25,5 @@ n, m = map(int, input().split())
 matrixA = fill_matrix(n)
 input()
 m, k = map(int, input().split())
-matrixB = fill_matrix(m)
+matrixB = flip_matrix(fill_matrix(m), m, k)
 print_matrix(mult_matrix(matrixA, matrixB, n, m, m, k))
