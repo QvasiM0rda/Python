e359c439b710c6ae91bb3c8c43fc740c7c97b589
QvasiM0rda <darkrustam@gmail.com>
2022-09-05 22:57:36 +0300

    matrix_multiply finalyzed, added matrix_power and one example for chapter 4

--- Stepik/matrix_power.py
+++ Stepik/matrix_power.py
@@ -1,4 +1,4 @@
-def fill_matrix(rows: int) -> list :
+def fill_matrix(rows: int) -> list:
     """ Возвращает матрицу, заполненную данными, введёнными пользователем """
     return [[int(i) for i in input().split()] for _ in range(rows)]
 
@@ -9,7 +9,13 @@ def mult_matrix(matrix1: list, matrix2: list, rows1: int, cols1: int, cols2: int
         for j in range(cols1):
             for k in range(cols2):
                 result[i][k] += matrix1[i][j] * matrix2[j][k]
+    return result
 
+def power_matrix(matrix: list, power: int, size: int) -> list:
+    """ Возводит матрицу в степень """
+    result = matrix.copy()
+    for _ in range(power - 1):
+        result = mult_matrix(result, matrix, size, size, size)
     return result
 
 def print_matrix(matrix : list):
@@ -17,9 +23,7 @@ def print_matrix(matrix : list):
     for row in matrix:
         print(*row)
 
-n, m = map(int, input().split())
-matrix_1 = fill_matrix(n)
-input()
-m, k = map(int, input().split())
-matrix_2 = fill_matrix(m)
-print_matrix(mult_matrix(matrix_1, matrix_2, n, m, k))
+n = int(input())
+matrix = fill_matrix(n)
+m = int(input())
+print_matrix(power_matrix(matrix, m, n))
