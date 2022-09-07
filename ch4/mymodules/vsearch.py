f78403d604fcd7d7105dedfd83d2b727e38a478a
QvasiM0rda <darkrustam@gmail.com>
2022-09-07 06:47:45 +0300

    mymodules directory moved to ch4, vsearch.py tested with pycodestyle and edited.

--- ch4/mymodules/vsearch.py
+++ ch4/mymodules/vsearch.py
@@ -1,10 +1,10 @@
-def search_for_vowels(phrase:str) -> set:
+def search_for_vowels(phrase: str) -> set:
     """ Выводит гласные, найденные во введённой фразе. """
     vowels = set('aeiou')
-    
+
     return vowels.intersection(set(phrase))
 
 
-def search_for_letters(phrase:str, letters:str='aeiou') -> set:
+def search_for_letters(phrase: str, letters: str = 'aeiou') -> set:
     """ Возвращает множество букв из 'letters', найденных в 'phrase' """
     return set(letters).intersection(set(phrase))
