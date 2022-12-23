2d76a51b8513f3305f5fccc19761a4c23482f630
qvasim0rda <darkrustam@gmail.com>
2022-12-23 15:35:42 +0300

    Телефонный справочник показывает данные из БД и позволяет внести новую запись.

--- phonebook/phonebook.py
+++ phonebook/phonebook.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from DBcm import UseDatabase
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ app.config['dbconfig'] = { 'host': '127.0.0.1',
 
 
 @app.route('/')
-def hello_world():
+def phonebook():
     with UseDatabase(app.config['dbconfig']) as cursor:
         _SQL = '''SELECT id, last_name, first_name, patronymic, department, post, phone_number
                   FROM users'''
@@ -18,6 +18,29 @@ def hello_world():
         content = cursor.fetchall()
         titles = ('Табельный номер', 'Фамилия', 'Имя', 'Отчество', 'Отдел', 'Должность', 'Внутренний номер')
         return render_template('phonebook.html', titles=titles, content=content)
+
+
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
+
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    id = request.form['id']
+    last_name = request.form['last_name']
+    first_name = request.form['first_name']
+    patronymic = request.form['patronymic']
+    department = request.form['department']
+    post = request.form['post']
+    phone_number = request.form['phone_number']
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = f'''INSERT INTO users (id, last_name, first_name, patronymic, department, post, phone_number)
+                  VALUES ({id}, '{last_name}', '{first_name}', '{patronymic}', '{department}', '{post}', '{phone_number}')'''
+        cursor.execute(_SQL)
+        
+    return redirect('http://127.0.0.1:5000/')
 
 
 app.run(debug=True)
